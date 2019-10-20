cd5833104b82606bd8bc92dee372dba094564a30
Alfredo <josalfk@hotmail.com>
2019-10-19 20:54:39 -0500

    Initialized model and posting classes

---
+++ Models/model.py
@@ -0,0 +1,7 @@
+class Model:
+    def __init__(self, postings, prices, default_commodity):
+        self.postings = postings
+        self.prices = prices
+        self.default_commodity = default_commodity
+
+
---
+++ Models/postings.py
@@ -0,0 +1,5 @@
+class Postings:
+    def __init__(self, date, description, lines):
+        self.date = date 
+        self.description = description
+        self.lines = lines
