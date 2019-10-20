28ea2194b8aac616347f04514e4ee1072e5f9a56
Alfredo <josalfk@hotmail.com>
2019-10-20 15:28:52 -0700

    Checking for file existence in parse_index function, and parsing modifications in the function

--- Ledger.py
+++ Ledger.py
@@ -1,20 +1,26 @@
 import Grammars.LedgerGrammar as ledgerParser
 import sys
 
-def parse_index(filepath):
-    files =[]
-    index = open(filepath,'r')
+def parse_index(indexPath):
+    filePaths =[]
+    index = open(indexPath,'r')
     for line in index:
-        if line != '\n':
-            result= line.strip()
-            result=result.split(" ")
+        result = line.strip("\n ")
+        result = result.split(" ")
+        if result[0] != '':
+            print("Result"+ str(result)) 
             if len(result) > 1:
-                if result[0] =='!include':
-                    files.append(result[1])
+                if result[0] == '!include':
+                    filePaths.append(result[1])
                 else:
                     pass
             else:
-                raise Exception("While parsing file {} \n directive '{}' requires an argument".format(filepath, result[0]))    
+                raise Exception("While parsing file {} \n directive '{}' requires an argument".format(indexPath, result[0]))    
+  
+    for filePath in filePaths:
+        if not os.path.isfile(filePath):
+            raise Exception("While parsing file {} \n file to include was not found '{}'".format(indexPath, filePath))
+    return filePaths
 
 
 while True:
