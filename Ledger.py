b0b203b16409ff87d6894cd82855285259b68194
Alfredo <josalfk@hotmail.com>
2019-10-19 22:50:00 -0500

    Created function to parse index.ledger

--- Ledger.py
+++ Ledger.py
@@ -1,6 +1,21 @@
 import Grammars.LedgerGrammar as ledgerParser
 import sys
 
+def parse_index(filepath):
+    files =[]
+    index = open(filepath,'r')
+    for line in index:
+        if line != '\n':
+            result= line.strip()
+            result=result.split(" ")
+            if len(result) > 1:
+                if result[0] =='!include':
+                    files.append(result[1])
+                else:
+                    pass
+            else:
+                raise Exception("While parsing file {} \n directive '{}' requires an argument".format(filepath, result[0]))    
+
 
 while True:
     userInput = raw_input("[] ")
