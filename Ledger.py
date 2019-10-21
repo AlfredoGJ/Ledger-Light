98b9aecb7c40b795e8eef526e7b4c805d40a35ca
Alfredo <josalfk@hotmail.com>
2019-10-21 02:24:06 -0700

    Created function to fetch a transaction list from parse nodes

--- Ledger.py
+++ Ledger.py
@@ -22,6 +22,38 @@ def parse_index(indexPath):
             raise Exception("While parsing file {} \n file to include was not found '{}'".format(indexPath, filePath))
     return filePaths
 
+def fetch_transactions(tree):
+    transactions=[]
+    for node in tree:
+        T={}
+        T["Date"] = node.elements[0].text
+        T["Description"] = node.elements[2].text
+        postings =[]
+        posting = {}
+
+        posting["Account"] = node.elements[5].elements[0].text
+        posting["Quantity"] = node.elements[5].elements[2].Quantity.text
+        posting["Commodity"] = node.elements[5].elements[2].Commodity.text
+        postings.append(posting)
+
+        for ap in node.elements[6]:
+            auxPosting = {}
+            auxPosting["Account"] = ap.elements[1].elements[0].text
+            
+            if len(ap.elements[1].elements[2].elements) != 0:
+                auxPosting["Quantity"] = ap.elements[1].elements[2].Quantity.text
+                auxPosting["Commodity"] = ap.elements[1].elements[2].Commodity.text
+
+            else:
+                auxPosting["Quantity"] = 0
+                auxPosting["Commodity"] = "$"
+
+            postings.append(auxPosting)
+            
+        T["Postings"] = postings
+        transactions.append(T)
+    return transactions
+
 
 while True:
     userInput = raw_input("[] ")
